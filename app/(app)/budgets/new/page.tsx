8f9496ce41b38534c3b303a0f2d29a8dfa71229b
Kian Bazarjani <kian@bazza.dev>
2024-05-18 21:52:46 -0400

    Use prefetching for time periods

--- app/(app)/budgets/new/page.tsx
+++ app/(app)/budgets/new/page.tsx
@@ -1,12 +1,27 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query'
 import NewBudgetForm from './new-budget-form'
+import { getTimePeriods } from '@/api/budgets'
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['time-periods'],
+    queryFn: () => getTimePeriods(),
+  })
+
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <h1 className="text-4xl font-semibold tracking-tighter">
         Create a new budget
       </h1>
-      <NewBudgetForm />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <NewBudgetForm />
+      </HydrationBoundary>
     </div>
   )
 }
