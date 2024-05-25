44a034c2e1186790361eba44a91b7f6ccac97cdf
Kian Bazarjani <kian@bazza.dev>
2024-05-24 22:10:23 -0400

    Add query provider, and RQ devtools

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -2,6 +2,8 @@ import Navbar from '@/components/layout/navbar'
 import { Toaster } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { getUser } from '@/lib/auth/actions'
+import QueryProvider from '@/providers/query-provider'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { redirect } from 'next/navigation'
 
 export default async function Layout({
@@ -16,14 +18,17 @@ export default async function Layout({
   }
 
   return (
-    <TooltipProvider>
-      <div className="flex min-h-screen flex-col p-4">
-        <Navbar />
-        <div className="mx-auto flex w-full max-w-screen-xl flex-1 flex-col py-10">
-          {children}
+    <QueryProvider>
+      <TooltipProvider>
+        <div className="flex min-h-screen flex-col p-4">
+          <Navbar />
+          <div className="mx-auto flex w-full max-w-screen-xl flex-1 flex-col py-10">
+            {children}
+          </div>
         </div>
-      </div>
-      <Toaster richColors />
-    </TooltipProvider>
+        <Toaster richColors />
+      </TooltipProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryProvider>
   )
 }
