6762d6677fb34fce69758d39847b45550b99af59
Kian Bazarjani <kian@bazza.dev>
2024-05-24 23:19:37 -0400

    Add more budget query factory methods, and transaction queries

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,5 +1,6 @@
 import { getBudgets } from '@/api/budgets'
 import { getTagsForBudget } from '@/api/tags'
+import { getTransaction } from '@/api/transactions'
 import { queryOptions } from '@tanstack/react-query'
 
 export const tagQueries = {
@@ -12,9 +13,25 @@ export const tagQueries = {
 }
 
 export const budgetQueries = {
-  all: () =>
+  all: () => ['budgets'],
+  list: () =>
     queryOptions({
-      queryKey: ['budgets'],
+      queryKey: ['budgets', 'list'],
       queryFn: () => getBudgets(),
+    }),
+  details: () => [...budgetQueries.all(), 'detail'],
+  detail: (id: number) =>
+    queryOptions({
+      queryKey: [...budgetQueries.details(), id],
+    }),
+}
+
+export const transactionQueries = {
+  all: () => ['transactions'],
+  details: () => [...transactionQueries.all(), 'detail'],
+  detail: (id: string) =>
+    queryOptions({
+      queryKey: [...transactionQueries.details(), id],
+      queryFn: () => getTransaction({ id }),
     }),
 }
