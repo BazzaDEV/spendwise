e35bf0cb1ac5d2b4d5e29e377186854b6dcc9426
Kian Bazarjani <kian@bazza.dev>
2024-06-08 15:04:17 -0400

    Add settings button to budget page

--- app/(app)/budgets/[id]/header.tsx
+++ app/(app)/budgets/[id]/header.tsx
@@ -1,22 +1,38 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 import { budgetQueries } from '@/lib/queries'
 import { cn } from '@/lib/utils'
 import { useSuspenseQuery } from '@tanstack/react-query'
+import { SettingsIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export function Header({ id }: { id: number }) {
+  const router = useRouter()
+
   const { data } = useSuspenseQuery(budgetQueries.list(id))
 
   return (
-    <h1
-      className={cn(
-        'inline-flex items-center gap-2',
-        'text-4xl font-semibold tracking-tighter',
-      )}
-    >
-      {data.name}
-    </h1>
+    <div className="flex items-center justify-between">
+      <h1
+        className={cn(
+          'inline-flex items-center gap-2',
+          'text-4xl font-semibold tracking-tighter',
+        )}
+      >
+        {data.name}
+      </h1>
+      <div>
+        <Button
+          className="gap-1.5"
+          variant="secondary"
+          onClick={() => router.push(`/budgets/${id}/settings`)}
+        >
+          <SettingsIcon className="size-4" /> Settings
+        </Button>
+      </div>
+    </div>
   )
 }
 
